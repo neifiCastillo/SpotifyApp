5937127a240beab355503801b42d31ddb1de12f3
neifi <neifi03128@gmail.com>
2021-01-18 17:56:31 -0500

    Proyecto terminado

--- src/app/pages/tab2/tab2.page.spec.ts
+++ src/app/pages/tab2/tab2.page.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
 
 import { Tab2Page } from './tab2.page';
 
@@ -11,8 +11,7 @@ describe('Tab2Page', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Tab2Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
-    }).compileComponents();
+     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab2Page);
     component = fixture.componentInstance;
